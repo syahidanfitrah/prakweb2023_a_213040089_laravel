12529c58dabf3b0076d883795300301879d0c8dc
syahidanfitrah <syahidanfitrah9@gmail.com>
2023-11-12 16:33:41 +0700

    Video ke 13

--- coba-laravel/routes/web.php
+++ coba-laravel/routes/web.php
@@ -21,13 +21,15 @@ use Illuminate\Routing\Route as RoutingRoute;
 
 Route::get('/', function () {
     return view('home', [
-        "title" => "Home"
+        "title" => "Home",
+        "active" => 'home'
     ]);
 });
 
 Route::get('/about', function () {
     return view('about', [
         "title" => 'About',
+        "active" => 'about',
         "name" => "Syahidan Fitrah",
         "email" => "syahidan@gmail.com",
         "image" => "esa.jpg"
@@ -47,17 +49,19 @@ Route::get('/categories', function() {
     ]);
 });
 
-Route::get('/categories/{category:slug}', function(Category $category) {
-    return view('posts', [
-        'title' => "Posts By Category : $category->name",
-        'active' => 'categories',
-        'posts' => $category->posts->load('category', 'author')
-    ]);
-});
 
-Route::get('/authors/{author:username}', function(User $author) {
-    return view('posts', [
-        'title' => "Post By Author : $author->name",
-        'posts' => $author->posts->load('category', 'author')
-    ]);
-});
+// Route::get('/categories/{category:slug}', function(Category $category) {
+//     return view('posts', [
+//         'title' => "Posts By Category : $category->name",
+//         'active' => 'categories',
+//         'posts' => $category->posts->load('category', 'author')
+//     ]);
+// });
+//
+// Route::get('/authors/{author:username}', function(User $author) {
+//     return view('posts', [
+//         'title' => "Post By Author : $author->name",
+//         'active' => 'posts',
+//         'posts' => $author->posts->load('category', 'author')
+//     ]);
+// });
