2bf13b229345818f1f4f36d586de1d2ab6ffbfab
syahidanfitrah <syahidanfitrah9@gmail.com>
2023-11-27 19:55:25 +0700

    Video ke 20

--- coba-laravel/resources/views/dashboard/posts/show.blade.php
+++ coba-laravel/resources/views/dashboard/posts/show.blade.php
@@ -7,12 +7,14 @@
       <h1 class="mb-3">{{ $post->title }}</h1>
 
       <a href="/dashboard/posts" class="btn btn-success"><span data-feather="arrow-left"></span> Back to my Posts</a>
-      <a href="" class="btn btn-warning"><span data-feather="edit"></span>Edit</a>
-      <a href="" class="btn btn-danger"><span data-feather="x-circle"></span> Delete</a>
+      <a href="/dashboard/posts/{{ $post->slug }}/edit" class="btn btn-warning"><span data-feather="edit"></span>Edit</a>
+      <form action="/dashboard/posts/{{ $post->slug }}" method="post" class="d-inline">
+        @method('delete')
+        @csrf
+        <button class="btn btn-danger" onclick="return confirm('Are you sure ?')"><span data-feather="x-circle"></span> Delete</button>
+      </form>
 
-      <p>By. <a href="/posts?author={{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/posts?category={{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
-
-      <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid">
+      <img src="https://source.unsplash.com/1200x400?{{ $post->category->name }}" alt="{{ $post->category->name }}" class="img-fluid mt-3">
 
       <article class="my-3 fs-5">
         {!! $post->body !!}
